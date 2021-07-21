ecb60694365412fc32aaf2ea671974baba10ecc4
maksims <a2320232@mail.ru>
2021-07-21 22:43:02 +0500

    0.9 bottom menu

--- buildozer-32bit.spec
+++ buildozer-32bit.spec
@@ -1,7 +1,7 @@
 [app]
 
 # (str) Title of your application
-title = Mortgage Calculator
+title = Cactus Coffee
 
 # (str) Package name
 package.name = mortgagecalculator
@@ -28,7 +28,7 @@ source.dir = .
 #source.exclude_patterns = license,images/*/*.jpg
 
 # (str) Application versioning (method 1)
-version = 0.8
+version = 0.9
 
 # (str) Application versioning (method 2)
 # version.regex = __version__ = ['"](.*)['"]
--- main.py
+++ main.py
@@ -1,22 +1,54 @@
-from kivy.uix.screenmanager import Screen
-
 from kivymd.app import MDApp
-from kivymd.uix.button import MDRectangleFlatButton
+from kivy.lang import Builder
 
 
-class MainApp(MDApp):
+class Test(MDApp):
+
     def build(self):
-        self.theme_cls.theme_style = "Dark"  # "Light"
         self.theme_cls.primary_palette = "Green"
+        self.theme_cls.theme_style = "Dark"
+        return Builder.load_string(
+            '''
+BoxLayout:
+    orientation:'vertical'
 
-        screen = Screen()
-        screen.add_widget(
-            MDRectangleFlatButton(
-                text="Hello, Усачишка! Как тебе кактусовая заставка?",
-                pos_hint={"center_x": 0.5, "center_y": 0.5},
-            )
+    MDToolbar:
+        title: 'Cactus Coffee'
+        md_bg_color: .2, .2, .2, 1
+        specific_text_color: 1, 1, 1, 1
+        
+
+    MDBottomNavigation:
+        panel_color: .2, .2, .2, 1
+
+        MDBottomNavigationItem:
+            name: 'screen 1'
+            text: 'Coffees'
+            icon: 'coffee-outline'
+
+            MDLabel:
+                text: 'Here will be beautiful pictures of coffee'
+                halign: 'center'
+
+        MDBottomNavigationItem:
+            name: 'screen 2'
+            text: 'Sales'
+            icon: 'alarm-light-outline'
+
+            MDLabel:
+                text: 'Do not miss the opportunity'
+                halign: 'center'
+
+        MDBottomNavigationItem:
+            name: 'screen 3'
+            text: 'Contacts'
+            icon: 'phone-classic'
+
+            MDLabel:
+                text: 'Call us 88002000600'
+                halign: 'center'
+'''
         )
-        return screen
 
 
-MainApp().run()
+Test().run()
